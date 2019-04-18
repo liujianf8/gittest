4c33780f0f8d7f600c9ea4f14bc4a2f6b3117e93
liujianf8@sina.com <liujianf8@sina.com>
2019-04-18 15:53:23 +0800

    新增List方法测试类

---
+++ GithubDemo/src/com/test/ListMethodTest.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListMethodTest {
+
+	public static void main(String[] args) {
+		List<String> aList=new ArrayList<String>();
+		for(int i=0;i<102;i++){
+			aList.add(i+"");
+		}
+
+	}
+
+}
