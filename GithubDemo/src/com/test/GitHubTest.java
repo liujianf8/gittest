f288b9b504214a2514464f5bd766254e720aa3c4
liujianf8 <liujianf8@sina.com>
2019-04-24 12:14:10 +0800

    third commit

--- GithubDemo/src/com/test/GitHubTest.java
+++ GithubDemo/src/com/test/GitHubTest.java
@@ -5,6 +5,7 @@ public class GitHubTest {
 	public static void main(String[] args) {
 		System.out.println("Hello Github!!!");
 		System.out.println("liu liu is merging!");
+		System.out.println("liu liu is merging!");
 
 	}
 
