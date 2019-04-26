f7094dbef00d1abcf561a7d5135576cb8169d9b4
liujianf8 <liujianf8@sina.com>
2019-04-26 15:49:42 +0800

    "测试提交"

--- GithubDemo/src/com/test/GitHubTest.java
+++ GithubDemo/src/com/test/GitHubTest.java
@@ -3,7 +3,7 @@ package com.test;
 public class GitHubTest {
 
 	public static void main(String[] args) {
-		System.out.println("it's flow develop,will merge to master");
+		System.out.println("it's ");
 
 	}
 
